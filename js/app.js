4b26fd44ba03a2525a6d16702b026b877fc7613d
jaeveedee <jasonvd@gmail.com>
2016-04-21 16:32:42 -0400

    added the info window

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 
+var marker;
 
 function initMap() {
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-      center: {lat: 40.7059, lng: -74.1285},
-      zoom: 12
-    });
-  }
+  var myLatLng = {lat: 40.7059, lng: -74.1285};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng
+  });
+
+  marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: 'Hello World!'
+
+  });
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Title Text</h1>'+
+      '<div id="bodyContent">'+
+      '<p>tweets in here.....</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  
+}
+
