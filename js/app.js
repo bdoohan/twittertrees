6a2e571218f3859773c77870240d70dcb2a3834c
jaeveedee <jasonvd@gmail.com>
2016-05-27 14:22:04 -0400

    added a blog page

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ $("#menu-bars a").on("click", function(){
 	$("#mobile-nav .menu-item").toggle();
 
 });
+
+
+
+///////for the iframe content
+
+$('iframe').load( function() {
+    $('iframe').contents().find("head")
+      .append($("<style type='text/css'>  .tumblr_posts {background-color: green !important;}   </style>"));
+});
